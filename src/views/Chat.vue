e035e1f90ec006f9bb7ecb966058b188b40f08a9
Hamzah Habibi <44396732+cubefreaker@users.noreply.github.com>
2021-06-20 00:39:13 +0700

    fix pre-line

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -463,11 +463,13 @@ export default {
 }
 
 #textLeft {
-  margin-bottom: 1rem;
+  margin: -1.5rem 0 1rem 0;
+  white-space: pre-line;
 }
 
 #textRight {
-  margin-bottom: 1rem;
+  margin: -1.5rem 0 1rem 0;
+  white-space: pre-line;
 }
 
 #delBtn {
